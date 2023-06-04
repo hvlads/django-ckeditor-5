5c4ca7d78dfd1d8a4cc4064eb0dc4ccd1ef5354e
Vladislav <vladislah@gmail.com>
2023-06-04 16:21:22 +0300

    Add pytest for example project.

--- example/blog/tests/test_storage.py
+++ example/blog/tests/test_storage.py
@@ -1,38 +1,32 @@
 import pytest
-from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
+from django.test import override_settings
 from django.utils.module_loading import import_string
 
 from django_ckeditor_5.views import get_storage_class
 
 
-def test_get_storage_class():
+@override_settings(
+    CKEDITOR_5_FILE_STORAGE="articles.storage.CustomStorage",
+    DEFAULT_FILE_STORAGE="django.core.files.storage.FileSystemStorage",
+    STORAGES={"default": {"BACKEND": "django.core.files.storage.FileSystemStorage"}},
+)
+def test_get_storage_class(settings):
     # Case 1: CKEDITOR_5_FILE_STORAGE is defined
-    storage_setting = "articles.storage.CustomStorage"
-    settings.CKEDITOR_5_FILE_STORAGE = storage_setting
     storage_class = get_storage_class()
-    assert storage_class == import_string(storage_setting)
+    assert storage_class == import_string(settings.CKEDITOR_5_FILE_STORAGE)
 
     # Case 2: DEFAULT_FILE_STORAGE is defined
-    default_storage_setting = "django.core.files.storage.FileSystemStorage"
-    settings.CKEDITOR_5_FILE_STORAGE = None
-    settings.DEFAULT_FILE_STORAGE = default_storage_setting
+    delattr(settings, "CKEDITOR_5_FILE_STORAGE")
     storage_class = get_storage_class()
-    assert storage_class == import_string(default_storage_setting)
+    assert storage_class == import_string(settings.DEFAULT_FILE_STORAGE)
 
     # Case 3: STORAGES['default'] is defined
-    default_storage_name = "django.core.files.storage.FileSystemStorage"
-    settings.DEFAULT_FILE_STORAGE = None
-    settings.STORAGES = {"default": {"BACKEND": default_storage_name}}
+    delattr(settings, "DEFAULT_FILE_STORAGE")
     storage_class = get_storage_class()
-    assert storage_class == import_string(default_storage_name)
+    assert storage_class == import_string(settings.STORAGES["default"]["BACKEND"])
 
     # Case 4: None of the required settings is defined
-    settings.STORAGES = {}
+    delattr(settings, "STORAGES")
     with pytest.raises(ImproperlyConfigured):
         get_storage_class()
-
-    # Clean up the settings
-    delattr(settings, "CKEDITOR_5_FILE_STORAGE")
-    delattr(settings, "DEFAULT_FILE_STORAGE")
-    delattr(settings, "STORAGES")
--- example/blog/tests/test_upload_file.py
+++ example/blog/tests/test_upload_file.py
@@ -11,10 +11,6 @@ def test_upload_file(admin_client, file):
 
 
 def test_upload_file_to_google_cloud(admin_client, file, settings, mocker):
-    m = mocker.patch("storages.backends.gcloud.Client")
-    m.return_value.bucket.return_value.blob.return_value.generate_signed_url.return_value = (
-        "new_url"
-    )
     settings.CKEDITOR_5_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
     settings.GS_BUCKET_NAME = "test"
     with file as upload:
@@ -23,4 +19,3 @@ def test_upload_file_to_google_cloud(admin_client, file, settings, mocker):
         )
     assert response.status_code == 200
     assert "url" in response.json()
-    assert response.json()["url"] == "new_url"
