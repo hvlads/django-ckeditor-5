0115efbd1965dd5ef2d299b7d0637d9cfcdf4652
hvlads <vladislah@gmail.com>
2024-04-21 21:44:00 +0300

    Add support for Full page HTML (#211)

--- django_ckeditor_5/static/django_ckeditor_5/src/ckeditor.js
+++ django_ckeditor_5/static/django_ckeditor_5/src/ckeditor.js
@@ -45,6 +45,7 @@ import { Style } from '@ckeditor/ckeditor5-style';
 import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
 import {LinkImage} from "@ckeditor/ckeditor5-link";
 import {HtmlEmbed} from "@ckeditor/ckeditor5-html-embed";
+import { FullPage } from '@ckeditor/ckeditor5-html-support';
 
 export default class ClassicEditor extends ClassicEditorBase {
 }
@@ -94,5 +95,6 @@ ClassicEditor.builtinPlugins = [
     Style,
     HorizontalLine,
     LinkImage,
-    HtmlEmbed
+    HtmlEmbed,
+    FullPage
 ];
