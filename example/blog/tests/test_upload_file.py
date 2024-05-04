a4e3055c29ca5cac0040b8ade31d25b5ac05299d
hvlads <vladislah@gmail.com>
2024-05-04 23:09:24 +0300

    Implemented custom upload functionality and updated documentation (#218)

--- example/blog/tests/test_upload_file.py
+++ example/blog/tests/test_upload_file.py
@@ -1,10 +1,12 @@
+from django.conf import settings
 from django.urls import reverse
 
 
 def test_upload_file(admin_client, file):
     with file as upload:
+        upload_view_name = getattr(settings, "CK_EDITOR_5_UPLOAD_FILE_VIEW_NAME", "")
         response = admin_client.post(
-            reverse("ck_editor_5_upload_file"),
+            reverse(upload_view_name),
             {"upload": upload},
         )
     assert response.status_code == 200
