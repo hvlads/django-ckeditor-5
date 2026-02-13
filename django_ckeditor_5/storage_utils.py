af83ce37820d3dddf9515dd24f25ad98bb99fc81
alexkiro <1538458+alexkiro@users.noreply.github.com>
2026-02-13 12:32:49 +0200

    Fix getting the upload storage
    With the new STORAGES setting, dev can also
    provide attribute to init the class.
    So just initializing the class is not correct.
    
    Use the `default_storage` lazy object instead,
    which should work for django version >= 1.8.

--- django_ckeditor_5/storage_utils.py
+++ django_ckeditor_5/storage_utils.py
@@ -1,37 +1,17 @@
 from PIL import Image
-from django.core.exceptions import ImproperlyConfigured
+from django.core.files.storage import default_storage
 from django.conf import settings
 from django.utils.module_loading import import_string
 
 from django_ckeditor_5.exceptions import NoImageException
 
 
-def get_django_storage_class():
+def get_django_storage():
     storage_setting = getattr(settings, "CKEDITOR_5_FILE_STORAGE", None)
-    default_storage_setting = getattr(settings, "DEFAULT_FILE_STORAGE", None)
-    storages_setting = getattr(settings, "STORAGES", {})
-    default_storage_name = storages_setting.get("default", {}).get("BACKEND")
-
     if storage_setting:
-        return import_string(storage_setting)
-    elif default_storage_setting:
-        try:
-            return import_string(default_storage_setting)
-        except ImportError:
-            error_msg = f"Invalid default storage class: {default_storage_setting}"
-            raise ImproperlyConfigured(error_msg)
-    elif default_storage_name:
-        try:
-            return import_string(default_storage_name)
-        except ImportError:
-            error_msg = f"Invalid default storage class: {default_storage_name}"
-            raise ImproperlyConfigured(error_msg)
+        return import_string(storage_setting)()
     else:
-        error_msg = (
-            "Either CKEDITOR_5_FILE_STORAGE, DEFAULT_FILE_STORAGE, "
-            "or STORAGES['default'] setting is required."
-        )
-        raise ImproperlyConfigured(error_msg)
+        return default_storage
 
 
 def image_verify(f):
@@ -42,7 +22,6 @@ def image_verify(f):
 
 
 def handle_uploaded_file(f):
-    storage = get_django_storage_class()
-    fs = storage()
+    fs = get_django_storage()
     filename = fs.save(f.name, f)
     return fs.url(filename)
